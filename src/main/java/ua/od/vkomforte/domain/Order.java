35322c04d18a3d7a25604e66a14776112a8cd873
Dmytry <85928123+romanov-create@users.noreply.github.com>
2021-10-29 12:25:45 +0300

    add fields in Customer & Order

--- src/main/java/ua/od/vkomforte/domain/Order.java
+++ src/main/java/ua/od/vkomforte/domain/Order.java
@@ -1,4 +1,59 @@
 package ua.od.vkomforte.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private String name;
+
+    private String status;
+
+    private String cost;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", cost='" + cost + '\'' +
+                '}';
+    }
 }
