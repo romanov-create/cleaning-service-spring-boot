f9142eb2a30e25f39f2eb2e6e62a6612942233ed
Dmytry <85928123+romanov-create@users.noreply.github.com>
2021-12-21 19:36:35 +0200

    add customer & order restController

--- src/main/java/ua/od/vkomforte/controllers/order/OrderRestControllerImpl.java
+++ src/main/java/ua/od/vkomforte/controllers/order/OrderRestControllerImpl.java
@@ -1,4 +1,46 @@
 package ua.od.vkomforte.controllers.order;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ua.od.vkomforte.domain.Customer;
+import ua.od.vkomforte.domain.Order;
+import ua.od.vkomforte.repository.CustomerRepository;
+import ua.od.vkomforte.repository.OrderRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api",produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderRestControllerImpl {
+    private final OrderRepository orderRepository ;
+
+    public OrderRestControllerImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    //Операция сохранения юзера в базу данных
+    @PostMapping("/orders")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Order saveOrder(@RequestBody Order order) {
+
+        return orderRepository.save(order);
+    }
+
+    //Получение списка юзеров
+    @GetMapping("/orders")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Order> getAllOrders() {
+
+        return orderRepository.findAll();
+    }
+
+    //Получения юзера по id
+    @GetMapping("/orders/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Order getOrderById(@PathVariable long id) {
+
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Entity with id = Not found"));
+    }
 }
